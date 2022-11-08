83a7e53898e6cc81366740570424bc4c50fd7e04
Erik Boye Sørensen <69841837+erikboye@users.noreply.github.com>
2022-11-08 22:10:05 +0100

    oddformat completed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import oddNumbs from '../pages/oddformat'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import OddNumbs from "../pages/oddformat";
 
 export default function Home() {
   return (
@@ -13,9 +13,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-      
-      <h1>Ruter</h1>
-      <oddNumbs/>
+        <h1>Ruter</h1>
+        <OddNumbs />
       </main>
 
       <footer className={styles.footer}>
@@ -24,12 +23,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
--- pages/oddformat.js
+++ pages/oddformat.js
@@ -1,20 +1,16 @@
 export default function oddNumbs(numbers) {
-    var count = 0;
-    for(var i = 0; i<numbers.length; i++){
-      for(var j = 0; j<numbers.length; j++){
-        if(numbers[i] == numbers[j]){
-          count++;
-        }
-      }
-      if(count % 2 != 0 ){
-        return numbers[i];
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (numbers[i] === numbers[j]) {
+        count++;
+        console.log("jegerlik", count, numbers[i]);
       }
     }
-  };
+    if (count % 2 !== 0) {
+      return numbers[i];
+    }
+  }
+}
 
-
-  console.log(oddNumbs([1, 1, 2, 2, 2, 2, 3, 3, 99 , 5, 5, 23, 23, 100, 100]));
-
-
-
-  
+console.log(oddNumbs([1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5, 23, 23, 100, 100]));
